68000deb32f32d45369ff84ffd510e36a96f6893
arda <ardackgz@gmail.com>
2023-09-26 22:26:28 +0300

    E-commerce Initial Commit

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { PRODUCTS } from '../../products'
+import { Product } from './product'
+import "./shop.css"
+
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+
+
+
+      <div className='shopTitle'>
+
+        <h1> Yetişen Alır</h1>
+
+      </div>
+
+      <div className='products'> {""}
+      {PRODUCTS.map((product) => (<Product data={product} />))}
+
+
+
+
+      </div>
+
+
+    </div>
+  )
+}
